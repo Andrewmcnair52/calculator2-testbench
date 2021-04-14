0d3abbef30e3f7b6e54ae2d14d8196df5ea900ef
Andrew McNair <andrew-52@hotmail.com>
2021-04-14 10:34:12 -0400

    fixes

--- calc2_tb.sv
+++ calc2_tb.sv
@@ -124,13 +124,13 @@ end
 
 //debug info
 initial begin
-  forever
-    @(negedge c_clk);
-    $display("req1_cmd_in: %h     out_resp1: %h", req1_cmd_in, out_resp1);
-		$display("req1_data_in: %h    out_data1: %h", req1_data_in, out_data1);
-		$display("req1_tag_in: %h     out_tag1: %h", req1_tag_in, out_tag1);
-		$display("time: %t            reset: %h", $time, reset);
-		$display();
+  always@(negedge c_clk) begin
+      $display("req1_cmd_in: %h     out_resp1: %h", req1_cmd_in, out_resp1);
+		  $display("req1_data_in: %h    out_data1: %h", req1_data_in, out_data1);
+		  $display("req1_tag_in: %h     out_tag1: %h", req1_tag_in, out_tag1);
+		  $display("time: %t            reset: %h", $time, reset);
+		  $display();
+  end
 end
 
 
