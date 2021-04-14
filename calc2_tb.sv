3dec8808be599425aac23a272d91491e1acccfac
Andrew McNair <andrew-52@hotmail.com>
2021-04-14 13:00:10 -0400

    fixes

--- calc2_tb.sv
+++ calc2_tb.sv
@@ -41,7 +41,7 @@ initial begin
   $display();
   
   transaction myTr; 
-  myTr = new( .p11(32'h10), .c1(4'h1), .t1(2'h2));
+  myTr = new( .p11(32'h10), .c1(4'h1), .t1(2'h2) );
   myTr.print();
 
 /*
