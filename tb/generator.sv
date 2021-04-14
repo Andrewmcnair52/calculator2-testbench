4f60ba97055e6dda02aa3e55363e77f0e5b0ef82
Andrew McNair <andrew-52@hotmail.com>
2021-04-14 15:02:12 -0400

    fixes

--- tb/generator.sv
+++ tb/generator.sv
@@ -1,7 +1,6 @@
 
 class Generator;  //class to generate transactions
 
-  transaction t;
   transaction trans_queue[$]; //transaction list
   
   //how we give generator tests? declare in constructor?
@@ -10,6 +9,7 @@ class Generator;  //class to generate transactions
   
   function new(); //generator constructor
     //a temporary test transaction
+    transaction t;
     t = new( .p11(32'h10), .p21(32'h11), .c1(4'h1), .t1(2'h2) );  //add 16+17 with tag=2
     t.setExpected();
     trans_queue.push_back(t);
