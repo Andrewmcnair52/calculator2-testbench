970a8a7dfbd873d90285740234f58e188070cb6c
Andrew McNair <andrew-52@hotmail.com>
2021-04-15 09:16:16 -0400

    added interface connected interface to DUT

--- calc2_tb.sv
+++ calc2_tb.sv
@@ -1,37 +1,15 @@
 `include "tb/transaction.sv"
 `include "tb/generator.sv"
 `include "tb/driver.sv"
+`include "tb/calc_if.sv"
 
 module calc2_tb;
 	
-  bit 			  c_clk = 0;
-	bit     		reset = 0;
-	
-	bit [3:0] 		req1_cmd_in   = 4'h0;
-	bit [31:0] 		req1_data_in  = 32'h0;
-	bit [1:0]     req1_tag_in 	= 2'h0;
-	bit [3:0] 		req2_cmd_in   = 4'h0;
-	bit [31:0] 		req2_data_in  = 32'h0;
-	bit [1:0]     req2_tag_in	  = 2'h0;
-	bit [3:0] 		req3_cmd_in   = 4'h0;
-	bit [31:0] 		req3_data_in  = 32'h0;
-	bit [1:0]     req3_tag_in	  = 2'h0;
-	bit [3:0] 		req4_cmd_in   = 4'h0;
-	bit [31:0] 		req4_data_in  = 32'h0;
-	bit [1:0]     req4_tag_in   = 2'h0;
-
-	bit [1:0]		out_resp1;
-	bit [31:0]	out_data1;
-	bit [1:0]   out_tag1;
-	bit [1:0]		out_resp2;
-	bit [31:0]	out_data2;
-	bit [1:0]   out_tag2;
-	bit [1:0]		out_resp3;
-	bit [31:0]	out_data3;
-	bit [1:0]   out_tag3;
-	bit [1:0]		out_resp4;
-	bit [31:0]	out_data4;
-	bit [1:0]   out_tag4;
+	//initialize clock
+  bit c_clk = 0;
+  
+  //define calculator interface
+  calc_if(c_clk);
 
   //command inputs:
   //Add: 4'h1   Sub: 4'h2
@@ -59,32 +37,32 @@ initial begin
 end
 
 calc2_top calc2_top(
-	.c_clk(c_clk),
-	.reset(reset),
-	.req1_cmd_in(req1_cmd_in),
-	.req1_data_in(req1_data_in),
-	.req1_tag_in(req1_tag_in),
-	.req2_cmd_in(req2_cmd_in),
-	.req2_data_in(req2_data_in),
-	.req2_tag_in(req2_tag_in),
-	.req3_cmd_in(req3_cmd_in),
-	.req3_data_in(req3_data_in),
-	.req3_tag_in(req3_tag_in),
-	.req4_cmd_in(req4_cmd_in),
-	.req4_data_in(req4_data_in),
-	.req4_tag_in(req4_tag_in),
-	.out_resp1(out_resp1),
-	.out_data1(out_data1),
-	.out_tag1(out_tag1),
-	.out_resp2(out_resp2),
-	.out_data2(out_data2),
-	.out_tag2(out_tag2),
-	.out_resp3(out_resp3),
-	.out_data3(out_data3),
-	.out_tag3(out_tag3),
-	.out_resp4(out_resp4),
-	.out_data4(out_data4),
-	.out_tag4(out_tag4)
+	.c_clk(calc_if.c_clk),
+	.reset(calc_if.reset),
+	.req1_cmd_in(calc_if.req1_cmd_in),
+	.req1_data_in(calc_if.req1_data_in),
+	.req1_tag_in(calc_if.req1_tag_in),
+	.req2_cmd_in(calc_if.req2_cmd_in),
+	.req2_data_in(calc_if.req2_data_in),
+	.req2_tag_in(calc_if.req2_tag_in),
+	.req3_cmd_in(calc_if.req3_cmd_in),
+	.req3_data_in(calc_if.req3_data_in),
+	.req3_tag_in(calc_if.req3_tag_in),
+	.req4_cmd_in(calc_if.req4_cmd_in),
+	.req4_data_in(calc_if.req4_data_in),
+	.req4_tag_in(calc_if.req4_tag_in),
+	.out_resp1(calc_if.out_resp1),
+	.out_data1(calc_if.out_data1),
+	.out_tag1(calc_if.out_tag1),
+	.out_resp2(calc_if.out_resp2),
+	.out_data2(calc_if.out_data2),
+	.out_tag2(calc_if.out_tag2),
+	.out_resp3(calc_if.out_resp3),
+	.out_data3(calc_if.out_data3),
+	.out_tag3(calc_if.out_tag3),
+	.out_resp4(calc_if.out_resp4),
+	.out_data4(calc_if.out_data4),
+	.out_tag4(calc_if.out_tag4)
 );
 
 
@@ -104,83 +82,6 @@ end
 		  $display();
   end
 */
-
-//=========================================================================================== Driver functions
-
- task automatic run_single(Transaction t);  //run a single transaction
-  
-    do_reset();
-    @(posedge c_clk);           //load in command, param1, and tag
-    req1_cmd_in   <= t.cmd[0];
-    req1_data_in  <= t.param1[0];
-    req1_tag_in   <= t.tag[0];
-    req2_cmd_in   <= t.cmd[1];
-    req2_data_in  <= t.param1[1];
-    req2_tag_in   <= t.tag[1];
-    req3_cmd_in   <= t.cmd[2];
-    req3_data_in  <= t.param1[2];
-    req3_tag_in   <= t.tag[2];
-    req4_cmd_in   <= t.cmd[3];
-    req4_data_in  <= t.param1[3];
-    req4_tag_in   <= t.tag[3];
-  
-    @(posedge c_clk);             //load in param2
-    req1_data_in  <= t.param2[0];
-    req2_data_in  <= t.param2[1];
-    req3_data_in  <= t.param2[2];
-    req4_data_in  <= t.param2[3];
-  
-    @(negedge c_clk);             //clear all inputs
-    req1_cmd_in   <= 4'h0;
-    req1_data_in  <= 32'h0;
-    req1_tag_in   <= 2'h0;
-    req2_cmd_in   <= 4'h0;
-    req2_data_in  <= 32'h0;
-    req2_tag_in   <= 2'h0;
-    req3_cmd_in   <= 4'h0;
-    req3_data_in  <= 32'h0;
-    req3_tag_in   <= 2'h0;
-    req4_cmd_in   <= 4'h0;
-    req4_data_in  <= 32'h0;
-    req4_tag_in   <= 2'h0;
-  
-    for(int i=0; i<t.clock_cycles; i++) begin		//give it specified number of clock cycles to respond
-	  	@(posedge c_clk);
-	  	if (out_tag1 == t.tag[0]) begin   //channel 1
-	  	  $display("channel 1 response after %0d cycles", i+1);
-	  	  t.data_out[0] = out_data1;
-	  	  t.resp_out[0] = out_resp1;
-	  	end
-	  	if (out_tag2 == t.tag[1]) begin   //channel 2
-	  	  $display("channel 2 response after %0d cycles", i+1);
-	  	  t.data_out[1] = out_data2;
-	  	  t.resp_out[1] = out_resp2;
-	  	end
-	  	if (out_tag3 == t.tag[2]) begin   //channel 3
-	  	  $display("channel 3 response after %0d cycles", i+1);
-	  	  t.data_out[2] = out_data3;
-	  	  t.resp_out[2] = out_resp3;
-	  	end
-	  	if (out_tag4 == t.tag[3]) begin   //channel 4
-	  	  $display("channel 4 response after %0d cycles", i+1);
-	  	  t.data_out[3] = out_data4;
-	  	  t.resp_out[3] = out_resp4;
-	  	end
-	  end
-
-  endtask
-
-
-
-  task do_reset;	//reset the device
-    begin
-      repeat(3) begin
-        @(posedge c_clk);
-        reset = 1;
-	    end
-	    @(posedge c_clk) reset = 0;
-	  end
-  endtask
 
 endmodule
 
--- tb/calc_if.sv
+++ tb/calc_if.sv
@@ -2,11 +2,36 @@
 `ifndef CALC_IF_DEFINE
 `define CALC_IF_DEFINE
 
+Interface calc_if (input bit c_clk);
 
+  logic     		reset = 0;
+	
+	logic [3:0] 		req1_cmd_in   = 4'h0;
+	logic [31:0] 		req1_data_in  = 32'h0;
+	logic [1:0]     req1_tag_in 	= 2'h0;
+	logic [3:0] 		req2_cmd_in   = 4'h0;
+	logic [31:0] 		req2_data_in  = 32'h0;
+	logic [1:0]     req2_tag_in	  = 2'h0;
+	logic [3:0] 		req3_cmd_in   = 4'h0;
+	logic [31:0] 		req3_data_in  = 32'h0;
+	logic [1:0]     req3_tag_in	  = 2'h0;
+	logic [3:0] 		req4_cmd_in   = 4'h0;
+	logic [31:0] 		req4_data_in  = 32'h0;
+	logic [1:0]     req4_tag_in   = 2'h0;
 
+	logic [1:0]		out_resp1;
+	logic [31:0]	out_data1;
+	logic [1:0]   out_tag1;
+	logic [1:0]		out_resp2;
+	logic [31:0]	out_data2;
+	logic [1:0]   out_tag2;
+	logic [1:0]		out_resp3;
+	logic [31:0]	out_data3;
+	logic [1:0]   out_tag3;
+	logic [1:0]		out_resp4;
+	logic [31:0]	out_data4;
+	logic [1:0]   out_tag4;
 
-
-
-
+Endinterface: calc_if 
 
 `endif
