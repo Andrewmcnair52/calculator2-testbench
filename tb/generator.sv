8ecb7334951bc68e1c9676e443869c74020f2dad
Andrew McNair <andrew-52@hotmail.com>
2021-04-17 08:23:19 -0400

    fixes

--- tb/generator.sv
+++ tb/generator.sv
@@ -17,7 +17,7 @@ class Generator;  //class to generate transactions
     trans_queue.push_back(t);
   endfunction
   
-  function add_continuous();
+  function add_continuous( input bit[31:0] p11=32'h0, p12=32'h0, p13=32'h0, p14=32'h0, p21=32'h0, p22=32'h0, p23=32'h0, p24=32'h0, input bit[3:0] c1=4'h0, c2=4'h0, c3=4'h0, c4=4'h0);
     t = new(p11,p12,p13,p14,p21,p22,p23,p24,c1,c2,c3,c4);
     t.setExpected();
     mbx.put(t);
