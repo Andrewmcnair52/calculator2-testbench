b0e60eb47b5e132d38c0b2e5894c8bbcecbbaf0c
Andrew McNair <andrew-52@hotmail.com>
2021-04-20 08:39:21 -0400

    fixes

--- tb/monitor.sv
+++ tb/monitor.sv
@@ -78,27 +78,26 @@ class Monitor;
         end else begin  //else run test
         
           @(posedge calc.c_clk);
-          $display("monitor: iteration %0d", i);
           if(calc.out_resp1!=0 && c1_received[calc.out_tag1]==0) begin   //if response on channel 1 and we havnt already seen that tag
-            $display("response on channel 1, tag %0d", calc.out_tag1);
+            $display("monitor iteration %0d: response on channel 1, tag %0d", i, calc.out_tag1);
             t.c1_out_resp[calc.out_tag1] = calc.out_resp1;
             t.c1_out_data[calc.out_tag1] = calc.out_data1;
             c1_received[calc.out_tag1] = 1;
           end
           if(calc.out_resp2!=0 && c2_received[calc.out_tag2]==0) begin  //if response on channel 2 and we havnt already seen that tag
-          $display("response on channel 2, tag %0d", calc.out_tag2);
+          $display("monitor iteration %0d: response on channel 2, tag %0d", i, calc.out_tag2);
             t.c2_out_resp[calc.out_tag2] = calc.out_resp2;
             t.c2_out_data[calc.out_tag2] = calc.out_data2;
             c2_received[calc.out_tag2] = 1;
           end
           if(calc.out_resp3!=0 && c3_received[calc.out_tag3]==0) begin  //if response on channel 3 and we havnt already seen that tag
-            $display("response on channel 3, tag %0d", calc.out_tag3);
+            $display("monitor iteration %0d: response on channel 3, tag %0d", i, calc.out_tag3);
             t.c3_out_resp[calc.out_tag3] = calc.out_resp3;
             t.c3_out_data[calc.out_tag3] = calc.out_data3;
             c3_received[calc.out_tag3] = 1;
           end
           if(calc.out_resp4!=0 && c4_received[calc.out_tag4]==0) begin  //if response on channel 4 and we havnt already seen that tag
-            $display("response on channel 4, tag %0d", calc.out_tag4);
+            $display("monitor iteration %0d: response on channel 4, tag %0d", i, calc.out_tag4);
             t.c4_out_resp[calc.out_tag4] = calc.out_resp4;
             t.c4_out_data[calc.out_tag4] = calc.out_data4;
             c4_received[calc.out_tag4] = 1;
