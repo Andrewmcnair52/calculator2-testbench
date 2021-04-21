9cdbfb2df626b8de739b30c4f852762cc0cc2421
Andrew McNair <andrew-52@hotmail.com>
2021-04-21 10:11:17 -0400

    fixes

--- tb/transaction.sv
+++ tb/transaction.sv
@@ -25,10 +25,10 @@ class Transaction;  //class to store info for a single test
   rand bit[3:0] c4_cmd[4];
   bit[1:0] c4_out_resp[4], c4_expected_resp[4];
   
-  constraint c1 {c1_cmd>0; c1_cmd<7; c1_cmd!=3; c1_cmd!=4;}
-  constraint c2 {c2_cmd>0; c2_cmd<7; c2_cmd!=3; c2_cmd!=4;}
-  constraint c3 {c3_cmd>0; c3_cmd<7; c3_cmd!=3; c3_cmd!=4;}
-  constraint c4 {c4_cmd>0; c4_cmd<7; c4_cmd!=3; c4_cmd!=4;}
+  constraint c1 { c1_cmd inside {[1,6]}; !(c1_cmd inside {[3,4]}); }
+  constraint c2 { c2_cmd inside {[1,6]}; !(c2_cmd inside {[3,4]}); }
+  constraint c3 { c3_cmd inside {[1,6]}; !(c3_cmd inside {[3,4]}); }
+  constraint c4 { c4_cmd inside {[1,6]}; !(c4_cmd inside {[3,4]}); }
   
   function print(); //display test data for debugging purposes
     $display("Ch1 param1: %h, %h, %h, %h", c1_param1[0],c1_param1[1],c1_param1[2],c1_param1[3]);
